f0fb0f765da1147f4289bc5620b2160e08d29a6e
mouthzipper <ramos.jeromeh@gmail.com>
2015-10-05 13:11:25 +0800

    Added add, edit and delete functionality

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -22,15 +22,17 @@ export default class App extends React.Component {
         }
       ]
     };
-
+    this.findNote = this.findNote.bind( this );
     this.addNote = this.addNote.bind( this );
+    this.editNote = this.editNote.bind( this );
+    this.deleteNote = this.deleteNote.bind( this );
   }
   render() {
     const notes = this.state.notes;
   	return (
   		<div>
   			<button className='add-note' onClick={this.addNote}>+</button>
-  			<Notes items={notes} />
+  			<Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
   		</div>
   	);
   }
@@ -42,5 +44,41 @@ export default class App extends React.Component {
   			task: 'New Task'
   		}])
   	})
+  }
+
+  editNote(id, task) {
+    let notes = this.state.notes;
+    const noteIndex = this.findNote( id );
+
+    if( noteIndex < 0 ) {
+      return;
+    }
+
+    notes[noteIndex].task = task;
+
+    this.setState( {notes} );
+  }
+
+  findNote( id ) {
+    const notes = this.state.notes;
+    const noteIndex = notes.findIndex( (note) => note.id === id );
+
+    if( noteIndex < 0 ) {
+      console.warn( 'Failed to find note', notes, id );
+    }
+
+    return noteIndex;
+  }
+
+  deleteNote( id ) {
+    const notes = this.state.notes;
+    const noteIndex = this.findNote(id);
+
+    if( noteIndex<0){
+      return;
+    }
+    this.setState({
+      notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
+    });
   }
 }
